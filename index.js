b22096261c97ceec37d0808169d9873aa85a704f
itzhaksh <123577657+itzhaksh@users.noreply.github.com>
2024-09-26 21:55:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ wss.on('connection', (ws) => {
     });
 });
 
-console.log(`WebSocket server is running on ws://localhost:8080`);
+console.log(`WebSocket server is running on ws://localhost:${process.env.PORT || 8080}`);
