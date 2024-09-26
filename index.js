8455fc025e6b15f356bbaf3ad473d2fb997e8c85
itzhaksh <123577657+itzhaksh@users.noreply.github.com>
2024-09-26 15:55:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ const WebSocket = require('ws');
 
 const messages = [];
 
-const ws = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
     console.log('Client connected via WebSocket');
 
-   ws.on('message', (message) => {
-    console.log('Received message:', message);
-    if (message === 'getMessages') {
-        const allMessages = [...messages];
-        messages.length = 0;
-        ws.send(JSON.stringify(allMessages));
-    } else {
-        try {
-            const data = JSON.parse(message);
-            if (data.message && data.value !== undefined) {
-                const newMessage = { message: data.message, value: data.value };
-                messages.push(newMessage);
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+        if (message === 'getMessages') {
+            const allMessages = [...messages];
+            messages.length = 0; 
+            ws.send(JSON.stringify(allMessages)); 
+        } else {
+            try {
+                const data = JSON.parse(message);
+                if (data.message && data.value !== undefined) {
+                    const newMessage = { message: data.message, value: data.value };
+                    messages.push(newMessage); 
 
-                wss.clients.forEach((client) => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify(newMessage));
-                    }
-                });
+                    wss.clients.forEach((client) => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify(newMessage));
+                        }
+                    });
+                }
+            } catch (error) {
+                console.error('Invalid message format:', error);
             }
-        } catch (error) {
-            console.error('Invalid message format:', error);
         }
-    }
+    });
 });
-
 
 console.log(`WebSocket server is running on ws://localhost:8080`);
