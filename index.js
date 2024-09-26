a6e0e9fcd60072c37d5ec6face3992a7f8144bbe
itzhaksh <123577657+itzhaksh@users.noreply.github.com>
2024-09-26 15:47:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,35 @@ const WebSocket = require('ws');
 
 const messages = [];
 
-const wss = new WebSocket.Server({ port: 8080 });
+const ws = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
     console.log('Client connected via WebSocket');
 
-    ws.on('message', (message) => {
-         console.log('Received message:', message);
-        if (message === 'getMessages') {
-            const allMessages = [...messages];
-            messages.length = 0;
-            ws.send(JSON.stringify(allMessages));
-        } else {
-            try {
-                const data = JSON.parse(message);
-                if (data.message && data.value !== undefined) {
-                    const newMessage = { message: data.message, value: data.value };
-                    messages.push(newMessage);
-                }
-            } catch (error) {
-                console.error('Invalid message format:', error);
+   ws.on('message', (message) => {
+    console.log('Received message:', message);
+    if (message === 'getMessages') {
+        const allMessages = [...messages];
+        messages.length = 0;
+        ws.send(JSON.stringify(allMessages));
+    } else {
+        try {
+            const data = JSON.parse(message);
+            if (data.message && data.value !== undefined) {
+                const newMessage = { message: data.message, value: data.value };
+                messages.push(newMessage);
+
+                wss.clients.forEach((client) => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify(newMessage));
+                    }
+                });
             }
+        } catch (error) {
+            console.error('Invalid message format:', error);
         }
-    });
+    }
 });
+
 
 console.log(`WebSocket server is running on ws://localhost:8080`);
